7af96a4b44c805bfba9fbc95876437cde45ca19c
ATREE Bangalore <70751764+atreebangalore@users.noreply.github.com>
2025-12-28 22:43:23 +0530

    spay9

--- app/components/History.tsx
+++ app/components/History.tsx
@@ -1,8 +1,9 @@
 'use client'
-import { collection, getDocs, Timestamp } from 'firebase/firestore';
+import { collection, deleteDoc, doc, getDocs, Timestamp } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'
 import { firestore } from './Firebase';
 import { format } from 'date-fns';
+import { Trash } from 'lucide-react';
 
 type ExpenseData = {
     amount: number;
@@ -25,6 +26,9 @@ type Expense = {
 const History = () => {
 
     const [expData, setExpData] = useState<Expense[]>([]);
+    const [change, setchange] = useState(false)
+    const[isdeletewindow, Setisdeletewindow]=useState(false)
+    const [deletedocid, setdeletedocid]=useState('')
 
 
     useEffect(() => {
@@ -35,7 +39,7 @@ const History = () => {
         };
 
         fetchData();
-    }, []);
+    }, [change]);
 
     const getdoc = async () => {
         const snapshot = await getDocs(collection(firestore, 'expenses'));
@@ -48,12 +52,42 @@ const History = () => {
                 b.data.createdAt.toMillis() - a.data.createdAt.toMillis()
         );
     }
+
+    const deletedoc = async (docID: string) => {
+        disableScroll()
+        setdeletedocid(docID)
+        Setisdeletewindow(true)
+
+      
+
+    }
+    const deletecnf=async()=>{
+          await deleteDoc(doc(firestore, "expenses", deletedocid));
+        setchange(!change)
+    }
+
+    const deletcancel=async()=>{
+         setdeletedocid('')
+         enableScroll();
+         Setisdeletewindow(false)
+
+        setchange(!change)
+    }
+    function disableScroll() {
+        document.body.style.overflow = 'hidden';
+    }
+
+    function enableScroll() {
+        document.body.style.overflow = '';
+    }
+
+
     return (
         <div className=' w-full max-w-sm mx-auto  '>
             <h1 className='flex text-lg font-semibold'>History</h1>
             <div>
                 {expData.map((exp, index) => (
-                    <div key={index} className='flex w-full border-2 shadow-xl my-2 rounded-xl py-4 flex-wrap justify-between text-sm px-2  '>
+                    <div key={index} className=' relative flex w-full border-2 shadow-xl my-2 rounded-xl py-4 flex-wrap justify-between text-sm px-2  '>
                         <div className="flex flex-col gap-2">
 
                             <span>{exp.data.name}</span>
@@ -62,10 +96,27 @@ const History = () => {
                         </div>
                         <span className="px-2 font-semibold text-xl">₹ {exp.data.amount}</span>
 
+                        <div className="absolute bottom-1 right-4 " onClick={() => { deletedoc(exp.id) }}>
+                            <Trash className="w-4 " />
+                        </div>
+
 
                     </div>
                 ))}
             </div>
+            {isdeletewindow&&<div className="absolute top-0 left-0 flex w-full h-[200vh] z-10   items-center justify-center px-8 ">
+                <div className="absolute top-0 left-0 flex w-full h-[200vh] z-1   items-center justify-center px-8 opacity-25  bg-black"></div>
+                <div className="w-full p-4 border-2 flex flex-col gap-2 rounded-xl items-center bg-white z-9">
+                    <h1 className="text-xl font-semibold py-2">Delete</h1>
+                    <p>Do you want to delete this?</p>
+                    <div className="flex justify-around gap-2 py-2">
+                        <button className="p-2 rounded-xl border-2" onClick={deletcancel}>Cancel</button>
+                        <button className="p-2 rounded-xl border-2 hover:bg-red-500 focus:bg-red-500 focus:border-red-300 focus:text-white" onClick={deletecnf}>Delete</button>
+                    </div>
+                </div>
+
+
+            </div>}
 
 
         </div>
